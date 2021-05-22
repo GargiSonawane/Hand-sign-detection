9e4c49524d8b74d2b9e51bef1b453910f3b66e4c
Gargi Sonawane <67555203+GargiSonawane@users.noreply.github.com>
2021-05-21 20:30:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,4 +19,37 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded(){
     console.log("Model Loaded");
 }
-
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "Ok sign"){
+            document.getElementById("update_emoji").innerHTML = "&#128076;"
+        }
+        if(results[0].label == "Peace sign"){
+            document.getElementById("update_emoji").innerHTML = "&#9996;"
+        }
+        if(results[0].label == "Thumbs Up"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;"
+        }
+        if(results[1].label == "Ok sign"){
+            document.getElementById("update_emoji2").innerHTML = "&#128076;"
+        }
+        if(results[1].label == "Peace sign"){
+            document.getElementById("update_emoji2").innerHTML = "&#9996;"
+        }
+        if(results[1].label == "Thumbs Up"){
+            document.getElementById("update_emoji2").innerHTML = "&#128077;"
+        }    
+    }
+}
